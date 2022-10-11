d0778d4e73954031feb2ab162c77fa072242b868
IgorP <yakotpoliglot@gmail.com>
2022-10-11 11:16:59 +0300

    login/signup_done

--- client/src/components/AuthModal.js
+++ client/src/components/AuthModal.js
@@ -1,20 +1,35 @@
 import { useState } from "react"
-
+import axios from 'axios'
+import { useHistory, useNavigate } from "react-router-dom";
+import { useCookies } from 'react-cookie'
 const AuthModal = ({ setShowModal, isSignUp }) => {
 
 
-
+    const history = useHistory()
     const handleClick = () => {
         setShowModal(false)
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         setError(null)
         e.preventDefault()
         if (isSignUp && (password !== confirmPassword)) {
             setError('Пароль не совпадает')
+            return
         }
-        else console.log('make a post request to our dataBase')
+        else {
+
+            const response = await axios.post(`http://localhost:8000/${isSignUp ? 'signup' : 'login'}`, { email, password })
+
+            setCookie('Email', response.data.email)
+            setCookie('UserId', response.data.user_id)
+            setCookie('Auth-token', response.data.token)
+            const success = response.status == 201
+
+            if (success && isSignUp) history.push('/onboarding')
+            if (success && !isSignUp) history.push('/dashboard')
+
+        }
     }
 
 
@@ -22,13 +37,13 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
     const [password, setPassword] = useState(null)
     const [confirmPassword, setConfirmPassword] = useState(null)
     const [error, setError] = useState(null)
-
+    const [cookies, setCookie, removeCookie] = useCookies(['user'])
 
 
     return (
         <div className="auth-modal">
             <div className="close-icon" onClick={handleClick} >&#10008;</div>
-          
+
             <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
             <p>By clicking submit you agree to the Terms and Conditions.</p>
             <form onSubmit={handleSubmit}>
--- server/index.js
+++ server/index.js
@@ -3,16 +3,87 @@ const express = require('express');
 const { MongoClient } = require('mongodb')
 const mongoose = require('mongoose')
 const uri = 'mongodb://localhost:27017'
-
-
+const { v1: uuidv4 } = require('uuid')
+const bcrypt = require('bcrypt')
 const app = express();
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+app.use(express.json())
+
+
+app.use(cors({
+    origin: true,
+    credentials: true,
+}));
+
 app.get('/', (req, res) => {
     res.json('Hello to my app')
 })
 
-app.post('/signup', (req, res) => {
-    res.json('This is ssignup route')
+
+app.post('/signup', async (req, res) => {
+
+    const client = new MongoClient(uri)
+
+    const { email, password } = req.body
+
+    const generateUserId = uuidv4()
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+
+    try {
+        await client.connect();
+        const database = client.db('app-data');
+        const users = database.collection('users');
+
+        const existingUser = await users.findOne({ email })
+        if (existingUser) { return res.status(409).send('user already exists. Please login') }
+
+
+        const sanitizedEmail = email.toLowerCase()
+
+        const data = {
+            user_id: generateUserId,
+            email: sanitizedEmail,
+            hashed_password: hashedPassword
+
+        }
+
+        const insertedUser = await users.insertOne(data)
+
+        const token = jwt.sign(insertedUser, sanitizedEmail, {
+            expiresIn: 60 * 24
+        })
+
+        res.status(201).json({ token, user_id: generateUserId, email: sanitizedEmail })
+    }
+    catch (error) { console.log(error) }
 })
+
+app.post('/login', async (req, res) => {
+    const client = new MongoClient(uri)
+    const { email, password } = req.body;
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const user = await users.findOne({ email })
+
+        const correctPasword = await bcrypt.compare(password, user.hashed_password)
+        if (user && correctPasword) {
+            const token = jwt.sign(user, email,
+                {
+                    expiresIn: 60 * 24
+                })
+            res.status(201).json({ token, userId: user.user_id, email })
+        }
+        res.status(400).send('invalid credentials')
+    }
+    catch (err) { console.log(err) }
+
+})
+
 
 app.get('/users', async (req, res) => {
     const client = new MongoClient(uri)
